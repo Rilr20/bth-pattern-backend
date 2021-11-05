75530ca9a0736dfc62c52c97d53d38b1cdf9cf0b
Rilr20 <nrrl99@gmail.com>
2021-11-05 16:30:32 +0100

    parkingspace_bike model changed

--- app/Models/Parkingspace_bike.php
+++ app/Models/Parkingspace_bike.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Parkingspace_bike extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'parkingspace_id',
+        'bike_id',
+        'arrival',
+        'departure'
+    ];
 }
