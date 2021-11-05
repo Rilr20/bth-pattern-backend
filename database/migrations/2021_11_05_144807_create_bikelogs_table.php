a5701614108338b48978f504facff6ad4d28d7cf
Rilr20 <nrrl99@gmail.com>
2021-11-05 16:07:42 +0100

    bikelog table created

---
+++ database/migrations/2021_11_05_144807_create_bikelogs_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBikelogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bikelogs', function (Blueprint $table) {
+            $table->id();
+            $table->float('start_x');
+            $table->float('start_y');
+            $table->dateTime('start_time');
+            $table->float('stop_x');
+            $table->float('stop_y');
+            $table->dateTime('stop_time');
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('bike_id');
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('bike_id')->references('id')->on('bikes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bikelogs');
+    }
+}
