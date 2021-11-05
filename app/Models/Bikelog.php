adbe221ff51765fb4f26a05f552a1778cf131b79
Rilr20 <nrrl99@gmail.com>
2021-11-05 16:17:10 +0100

    bike log changes

--- app/Models/Bikelog.php
+++ app/Models/Bikelog.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Bikelog extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'start_x',
+        'start_y',
+        'start_time',
+        'stop_x',
+        'stop_y',
+        'stop_time',
+        'customer_id',
+        'bike_id'
+    ];
 }
