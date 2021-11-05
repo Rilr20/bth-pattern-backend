f67443569b8b9c1a0228d65e1d05cb410848fcaf
Rilr20 <nrrl99@gmail.com>
2021-11-05 15:29:20 +0100

    user table changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,6 @@ class User extends Authenticatable
         'email',
         'password',
         'balance',
-        'subscription',
         'card_info',
         'csv',
         'expiration_date'
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('balance', 10)->default(0);
-            $table->tinyInteger('subscription')->default(0);
             $table->string('card_info', 16)->nullable();
             $table->string('csv', 3)->nullable();
             $table->string('expiration_date', 5)->nullable();
