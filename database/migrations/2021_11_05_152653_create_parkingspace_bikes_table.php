ee67ac710b60dfb4edaaac7fcf62868174acdf0a
Rilr20 <nrrl99@gmail.com>
2021-11-05 16:29:27 +0100

    parkingspace_bike is now created

---
+++ database/migrations/2021_11_05_152653_create_parkingspace_bikes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParkingspaceBikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parkingspace_bikes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('parkingspace_id');
+            $table->unsignedBigInteger('bike_id');
+            $table->dateTime('arrival');
+            $table->dateTime('departure');
+            $table->timestamps();
+
+            $table->foreign('parkingspace_id')->references('id')->on('parkingspaces')->onDelete('cascade');
+            $table->foreign('bike_id')->references('id')->on('bikes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parkingspace_bikes');
+    }
+}
